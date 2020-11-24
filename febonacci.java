12103b0f01730e4726b50afcf418cabd3964d810
GeorgeChijarira <gchijarira@gmail.com>
2020-11-24 15:15:38 +1100

    Add files via upload

---
+++ febonacci.java
@@ -0,0 +1,56 @@
+package firstJavaProject;
+import java.util.Scanner;
+
+public class febonacci {
+
+	
+	
+	private static void displaySequence(int fedoNum)
+	{
+		int n = 0;
+		int n1 = 0;
+		int n2 = 1;
+		int x;
+		
+		
+		if( fedoNum <= 0) 
+		{
+			System.out.println("input number greater than 0");
+			requestNumber();
+		}
+			
+			else 
+			{
+			
+				while( n < fedoNum )
+				{
+					System.out.println(n1 + "");
+					
+					x = n1;
+					n1 = n1 + n2;
+					n2 = x;
+					
+			
+					n++;
+				}
+			}
+	}
+	
+
+	public static void requestNumber() {
+		
+		 try (Scanner myObj = new Scanner(System.in)) {
+			System.out.println("Please input number...");
+
+		    int fedonachiNum = myObj.nextInt();  // Read user input
+	
+		displaySequence( fedonachiNum);
+		}
+		 catch (Exception e) {
+		      System.out.println("Something went wrong.");
+		    }
+	}
+	
+	
+
+}
---
+++ theFebonacciSequence.java
@@ -0,0 +1,10 @@
+package firstJavaProject;
+
+public class theFebonacciSequence {
+
+	public static void main(String[] args) {
+		
+		febonacci.requestNumber();
+	}
+
+}
